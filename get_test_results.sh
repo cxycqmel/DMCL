848a7697efece850d12579b3522d863a3f0b9018
cxycqmel <xcai@swin.edu.au>
2021-06-07 03:42:25 +1000

    util

---
+++ get_test_results.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for dir in `ls checkpoints`; do 
+  if [ -f  checkpoints/$dir/test_metrics.txt ]; then
+    echo "dir name: "$dir;  
+    python get_test_results.py checkpoints/$dir/test_metrics.txt;
+    echo "" 
+  fi
+done

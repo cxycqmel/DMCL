3d53dcd28955566aadf3e4d5b61be9c07691aa07
cxycqmel <xcai@swin.edu.au>
2021-06-07 15:27:29 +1000

    add print

--- get_test_results.sh
+++ get_test_results.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for dir in `ls checkpoints`; do 
-  if [ -f  checkpoints/$dir/test_metrics.txt ]; then
+  if [ -e  checkpoints/$dir/test_metrics.txt ] && [ -e checkpoints/$dir/metrics.txt ]; then
     echo "dir name: "$dir;  
     python get_test_results.py checkpoints/$dir/test_metrics.txt checkpoints/$dir/metrics.txt;
     echo "" 
